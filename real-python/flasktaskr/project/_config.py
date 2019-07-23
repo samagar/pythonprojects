596d2bfe1721222b6137bcaf43de1ba1ca918fd8
Sandeep Magar <Sandi.magar@gmaill.com>
2019-07-23 11:20:47 -0700

    sqlachmy

--- real-python/flasktaskr/project/_config.py
+++ real-python/flasktaskr/project/_config.py
@@ -11,3 +11,5 @@ SECRET_KEY = 'myprecious'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- real-python/flasktaskr/project/db_create.py
+++ real-python/flasktaskr/project/db_create.py
@@ -1,7 +1,10 @@
 # project/db_create.py
 
+from views import db
+from models import Task
+from datetime import date
 
-import sqlite3
+'''import sqlite3
 from _config import DATABASE_PATH
 
 with sqlite3.connect(DATABASE_PATH) as connection:
@@ -22,4 +25,14 @@ with sqlite3.connect(DATABASE_PATH) as connection:
     c.execute(
         'INSERT INTO tasks (name, due_date, priority, status)'
         'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-    )
+    )'''
+
+# create the database and the db table
+db.create_all()
+
+# insert data
+db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
+
+# commit the changes
+db.session.commit()
